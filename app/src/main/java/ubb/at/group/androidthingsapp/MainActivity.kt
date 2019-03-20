8896b90e6fb89367da7eef729be7e138dd2e3b37
Dávid Leiti <leiti26@gmail.com>
2019-03-20 17:00:15 +0200

    Added notifications when receiving data

--- app/src/main/java/ubb/at/group/androidthingsapp/MainActivity.kt
+++ app/src/main/java/ubb/at/group/androidthingsapp/MainActivity.kt
@@ -1,9 +1,13 @@
 package ubb.at.group.androidthingsapp
 
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.google.firebase.FirebaseApp
+import androidx.core.app.NotificationCompat
 import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +27,7 @@ class MainActivity : AppCompatActivity() {
             override fun onChildAdded(p0: DataSnapshot, p1: String?) {
                 val data = p0.getValue(SensorData::class.java)
                 Log.d("WaterSensorLogger", "Value received: $data")
+                sendNotification(data!!)
             }
 
             override fun onChildMoved(p0: DataSnapshot, p1: String?) = Unit
@@ -31,5 +36,29 @@ class MainActivity : AppCompatActivity() {
 
             override fun onChildRemoved(p0: DataSnapshot) = Unit
         })
+    }
+
+    private fun sendNotification(data: SensorData) {
+        val notificationManager = getSystemService(Service.NOTIFICATION_SERVICE) as NotificationManager
+        val actionIntent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+        }
+        val pendingIntent = PendingIntent.getActivity(this, 0, actionIntent, 0)
+
+        NotificationCompat.Builder(this, BaseApplication.CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Data received")
+            .setContentText("Rain: ${data.rain}, water level: ${data.waterLevel}")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+            .also { notification ->
+                notificationManager.notify(NOTIFICATION_ID, notification)
+            }
+    }
+
+    companion object {
+        private const val NOTIFICATION_ID = 100
     }
 }
--- app/src/main/java/ubb/at/group/androidthingsapp/SensorData.kt
+++ app/src/main/java/ubb/at/group/androidthingsapp/SensorData.kt
@@ -1,3 +1,3 @@
 package ubb.at.group.androidthingsapp
 
-data class SensorData(val rainData: String? = null, val waterData: String? = null)
+data class SensorData(val rain: String? = null, val waterLevel: String? = null)
