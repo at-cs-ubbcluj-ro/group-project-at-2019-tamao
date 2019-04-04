a8a9929b5ade5be84d67a662eb3e5b5fdacf0a96
Corina Todoran <todorananacorina13@gmail.com>
2019-04-04 10:14:10 +0300

    build details

--- at-site/src/components/Build.js
+++ at-site/src/components/Build.js
@@ -7,30 +7,17 @@ function Build(props) {
     <div className="Build" id="Build">
       <h2>{props.name}</h2>
       <Typography variant="body1" color="inherit" style={{ fontSize: 14 }}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Senectus et netus et
-        malesuada fames ac turpis egestas. Quam elementum pulvinar etiam non
-        quam. Tellus orci ac auctor augue mauris augue neque gravida. Fames ac
-        turpis egestas maecenas pharetra convallis posuere. Ut placerat orci
-        nulla pellentesque dignissim enim sit. Dictum at tempor commodo
-        ullamcorper a. Sodales ut eu sem integer. Id porta nibh venenatis cras
-        sed felis eget velit. Felis bibendum ut tristique et egestas quis.
-        Tortor aliquam nulla facilisi cras. Est sit amet facilisis magna etiam.
-        Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi
-        tristique senectus. Cursus in hac habitasse platea dictumst quisque
-        sagittis purus sit. Tempus egestas sed sed risus pretium quam. Sapien
-        nec sagittis aliquam malesuada bibendum arcu vitae. Scelerisque purus
-        semper eget duis. Enim nunc faucibus a pellentesque sit amet porttitor
-        eget. Elementum facilisis leo vel fringilla est ullamcorper eget. Cursus
-        risus at ultrices mi tempus. Hac habitasse platea dictumst quisque
-        sagittis purus sit amet. Metus vulputate eu scelerisque felis imperdiet
-        proin fermentum. Tellus rutrum tellus pellentesque eu tincidunt tortor
-        aliquam nulla facilisi. Etiam tempor orci eu lobortis elementum nibh
-        tellus molestie nunc. Tincidunt id aliquet risus feugiat in ante metus
-        dictum at. Turpis in eu mi bibendum. Blandit volutpat maecenas volutpat
-        blandit aliquam. Tincidunt tortor aliquam nulla facilisi cras. Integer
-        vitae justo eget magna fermentum iaculis eu non diam. Quam id leo in
-        vitae turpis. Potenti nullam ac tortor vitae purus faucibus ornare.
+        <p>
+          Developed an Android companion application in order to receive the information from the Raspberry Pi
+          sensors that are running in our main program. The sensor inputs along with the date are saved in a Firebase
+          database on Cloud. After a new input is added to the database our companion app pushes a notification to the user’s phone telling him the new state.
+          In order to connect the app to the Firebase database we performed the following steps:
+      </p>
+        <ul>
+          <li>Registered our app filling in the package name, the app nickname and the debugging signing certificate SHA-1;</li>
+          <li>Added the google-services.json to our project;</li>
+          <li>Added the plugins in the gradle file.</li>
+        </ul>
       </Typography>
     </div>
   );
